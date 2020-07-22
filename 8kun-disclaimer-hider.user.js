de8c56980372885e0c9cd42faa27268fb894e919
sn0w <27629096+2sn0w@users.noreply.github.com>
2020-07-21 22:52:47 -0400

    Hide the useless bar in the OP
    shitty bruteforce method but it works so idc

--- 8kun-disclaimer-hider.user.js
+++ 8kun-disclaimer-hider.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         8kun Disclaimer Hider
-// @namespace    https://github.com/2sn0w/8kun-disclaimer-hider/
+// @namespace    https://github.com/2sn0w/8kun-disclaimer-hider
 // @version      1.0
 // @description  Hide annoying disclaimer on 8kun.top
 // @author       sn0w
@@ -11,4 +11,20 @@
 (function() {
     'use strict';
     $('.disclaimer-8kun').hide()
+    var el, els = document.getElementsByTagName('*');
+    var node, nodes;
+    for (var i=0, iLen=els.length; i<iLen; i++) {
+      el = els[i];
+      if (el.tagName.toLowerCase() != 'script') {
+        nodes = el.childNodes;
+      } else {
+        nodes = [];
+      }
+      for (var j=0, jLen=nodes.length; j<jLen; j++) {
+          node = nodes[j];
+          if (node.nodeType == 3) {
+              node.data = node.data.replace('        ____________________________        ', '');
+          }
+      }
+    }
 })();
