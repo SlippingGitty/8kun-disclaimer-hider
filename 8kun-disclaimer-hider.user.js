3e20b49c822d6ed4814dbda6240c8e92149c65a6
sn0w <27629096+2sn0w@users.noreply.github.com>
2020-07-23 23:04:40 -0400

    hide even more stupid legal shit

--- 8kun-disclaimer-hider.user.js
+++ 8kun-disclaimer-hider.user.js
@@ -13,6 +13,10 @@
     $('.disclaimer-8kun').hide()
     $('section.col-12.col.box.description:nth-of-type(3)').hide()
     $('div.col-5.col:nth-of-type(3)').hide()
+    $('p.unimportant:nth-of-type(2)').hide()
+    $('p.unimportant:nth-of-type(3)').hide()
+    $('p.unimportant:nth-of-type(4)').hide()
+
     var el, els = document.getElementsByTagName('*');
     var node, nodes;
     for (var i=0, iLen=els.length; i<iLen; i++) {
