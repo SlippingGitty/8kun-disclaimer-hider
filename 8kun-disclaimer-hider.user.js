b962e4476664a50544c505f1c85d842a0c8785a4
SlippingGitty <76500838+SlippingGitty@users.noreply.github.com>
2021-01-15 11:38:30 -0600

    Removes jquery-3.4.1.min.js which broke things
    jquery-3.4.1.min.js is removed from @required as it broke certain 8kun functions in the Options menu.

--- 8kun-disclaimer-hider.user.js
+++ 8kun-disclaimer-hider.user.js
@@ -1,11 +1,10 @@
 // ==UserScript==
 // @name         8kun Disclaimer Hider
-// @namespace    https://github.com/2sn0w/8kun-disclaimer-hider
-// @version      1.0
-// @description  Hide annoying disclaimer on 8kun.top
-// @author       sn0w
+// @namespace    https://github.com/SlippingGitty/8kun-disclaimer-hider
+// @version      1.0.1
+// @description  Hide annoying disclaimers on 8kun.top
+// @author       sn0w, SlippingGitty
 // @match        http*://8kun.top/*
-// @require      https://code.jquery.com/jquery-3.4.1.min.js
 // ==/UserScript==
 
 (function() {
